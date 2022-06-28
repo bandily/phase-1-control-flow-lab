25d04dd5c8e12dbe043195feb1436231f1ca302e
Brian <huynh.sbrian@gmail.com>
2022-06-27 17:08:40 -0700

    Done with assignment

--- index.js
+++ index.js
@@ -1,11 +1,38 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(someValue){
+  var ride = 'This one is on me!';
+  var payTwenty = 'That will be twenty bucks.';
+  var payThirty = 'I will gladly take your thirty bucks.';
+  var noRide = 'No can do.';
+
+  if(someValue <= 400) {
+    return ride;
+
+  }else if(someValue <= 2000) {
+    return payTwenty;
+
+  }else if(someValue <= 2500) {
+    return payThirty;
+
+  }else if(someValue >= 2500) {
+    return noRide;
+  }
+  scuberGreetingForFeet(199);
+  scuberGreetingForFeet(1500);
+  scuberGreetingForFeet(2001);
+  scuberGreetingForFeet(2501);
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  return city === "NYC" ? "Ok, sounds good." : "No go.";
+
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(feeling) {
+  if(feeling === "generous"){
+    return 'Thank you so much.';
+  }else if(feeling === "not as generous"){
+    return 'Thank you.';
+  }else{
+    return 'Bye.';
+  }
 }
